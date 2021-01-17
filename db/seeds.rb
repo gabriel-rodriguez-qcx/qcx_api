d1f96d10fcdf293ca9c829983902fc75670055be
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2021-01-17 01:04:26 -0300

    Add Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,22 @@
+# rubocop:disable Rails/SkipsModelValidations
 # frozen_string_literal: true
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+
+Rails.logger.info 'Inserting questions'
+URI('https://raw.githubusercontent.com/qcx/desafio-backend/master/questions.json')
+  .then { |uri| Net::HTTP.get(uri) }
+  .then { |response| JSON.parse(response, symbolize_names: true) }
+  .tap { |qas| qas.each { |qa| qa.merge!(updated_at: Time.zone.now) } }
+  .in_groups_of(1000, false) { |question_accesses_attr| Question.insert_all(question_accesses_attr) }
+Rails.logger.info 'Done.'
+
+Rails.logger.info 'Inserting QuestionAccesses'
+URI('https://raw.githubusercontent.com/qcx/desafio-backend/master/question_access.json')
+  .then { |uri| Net::HTTP.get(uri) }
+  .then { |response| JSON.parse(response, symbolize_names: true) }
+  .tap { |qas| qas.each { |qa| qa.merge!(created_at: Time.zone.now, updated_at: Time.zone.now) } }
+  .in_groups_of(1000, false) { |question_accesses_attr| QuestionAccess.insert_all(question_accesses_attr) }
+Rails.logger.info 'Done.'
+
+# rubocop:enable Rails/SkipsModelValidations
