5cca17993c6f713135bd469de9b61a15593a565e
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2021-01-17 00:50:36 -0300

    Add scopes to QuestionAccess

--- app/models/question_access.rb
+++ app/models/question_access.rb
@@ -3,4 +3,15 @@ class QuestionAccess < ApplicationRecord
 
   validates :times_accessed, :date, presence: true
   validates :times_accessed, numericality: true
+
+  scope :by_year, ->(year) { where(date: Date.new(year.to_i)...Date.new(year.to_i + 1)) }
+  scope :by_month, lambda { |year, month|
+    where(date: Date.new(year.to_i, month.to_i)...Date.new(year.to_i, month.to_i).end_of_month)
+  }
+
+  scope :by_week, lambda { |year, week_number|
+    beginning_of_week = Date.commercial(year.to_i, week_number.to_i)
+
+    where(date: beginning_of_week...beginning_of_week + 7.days)
+  }
 end
