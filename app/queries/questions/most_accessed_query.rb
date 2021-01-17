86032893765aee3a893ef0674592ba2bb09bf1cd
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2021-01-17 00:51:12 -0300

    Add MostAccessedQuery

---
+++ app/queries/questions/most_accessed_query.rb
@@ -0,0 +1,29 @@
+module Questions
+  class MostAccessedQuery
+    def self.call(args)
+      new(args)
+        .query
+        .tap { |responses, meta| yield responses, meta if block_given? }
+    end
+
+    def initialize(args)
+      @year = args[:year]
+      @month = args[:month]
+      @week = args[:week]
+    end
+
+    attr_reader :year, :month, :week
+
+    def query
+      %w[week month year].each do |param|
+        value = send(param)
+        next unless value
+
+        @query = QuestionAccess.public_send("by_#{param}", *[year, value].uniq)
+        break
+      end
+
+      @query&.group(:question_id)&.order('sum_times_accessed desc')&.limit(10)&.sum(:times_accessed)
+    end
+  end
+end
