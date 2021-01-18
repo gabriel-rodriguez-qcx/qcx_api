417c013fc8a2f4df6181ffde801787233eadb8ae
Gabriel Rodriguez <gabriel.rdrguez@gmail.com>
2021-01-17 21:42:38 -0300

    Add cache to services

--- app/services/disciplines/fetch_most_accessed_service.rb
+++ app/services/disciplines/fetch_most_accessed_service.rb
@@ -4,14 +4,20 @@ module Disciplines
       new.perform
     end
 
-    def initialize
-      @most_accessed = Disciplines::MostAccessedQuery.call
-    end
-
     def perform
-      @most_accessed.map do |discipline, times_accessed|
+      most_accessed.map do |discipline, times_accessed|
         Discipline.new(name: discipline, times_accessed: times_accessed, id: discipline)
       end
+    end
+
+    private
+
+    def most_accessed
+      @most_accessed ||=
+        Rails.cache.fetch("#{self.class.name}##{__method__}",
+                          expires_in: Time.zone.now.at_end_of_day - Time.zone.now) do
+          Disciplines::MostAccessedQuery.call
+        end
     end
   end
 end
--- app/services/questions/fetch_most_accessed_service.rb
+++ app/services/questions/fetch_most_accessed_service.rb
@@ -5,13 +5,23 @@ module Questions
     end
 
     def initialize(args)
-      @most_accessed = Questions::MostAccessedQuery.call(args)
+      @args = args
     end
 
     def perform
-      questions = Question.find(@most_accessed.keys)
+      Rails.cache.fetch(cache_key(__method__, @args), expires_in: 1.day) do
+        questions = Question.find(most_accessed.keys)
 
-      [questions, @most_accessed]
+        [questions, most_accessed]
+      end
+    end
+
+    def most_accessed
+      @most_accessed ||= Questions::MostAccessedQuery.call(@args)
+    end
+
+    def cache_key(prefix, args)
+      "#{self.class.name}_#{prefix}_#{args.to_json}"
     end
   end
 end
